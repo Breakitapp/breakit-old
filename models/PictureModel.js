0c0d61b791f20e077fec699c9b0bc62d44ba2e45
Mikko Majuri <majuri.mikko@gmail.com>
2012-08-24 10:24:38 +0300

    media taggin search refinement

--- models/PictureModel.js
+++ models/PictureModel.js
@@ -105,8 +105,8 @@ var compare = function(a,b) {
 //Find stuff for media
 models.Picture.prototype.findHBL = function(callback) {
 	var hbl = [];
-	console.log('finding all with Red Bull-tag');
 	models.Picture.find({'tags':'Red Bull'}).sort("date","descending").exec(function(err, pics) {
+		console.log('finding all with Red Bull-tag');
 		if(err) throw err;
 		pics.forEach(function(pic) {
 			console.log(pic);
